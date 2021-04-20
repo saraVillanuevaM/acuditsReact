959c972fd3443e0ad12447cc10cd60d261b92a34
Sara Villanueva <sara.villanuevamonago@gmail.com>
2021-04-20 17:44:37 +0200

    first

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ export default () => {
 
     return (
         <div>
-            hello World!
+            
+           <h1> An joke a day keeps the doctor away</h1>
+ <button id="chiste">Next Joke</button>
+
         </div>
     );
 };
+
+
+// //  document.querySelector('#chiste').addEventListener ('click', () =>{
+//             fetch('https://icanhazdadjoke.com/slack')
+//             .then(res => res.json())
+//             .then(res=> document.querySelector('h3').textContent= res.attachments[0].text); 
+//         });
+        
