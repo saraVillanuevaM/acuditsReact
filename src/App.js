51366af61449e02b64809a2a07865950252d3ee1
Sara Villanueva <sara.villanuevamonago@gmail.com>
2021-04-27 19:23:18 +0200

    v2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,51 @@
-export default () => {
 
-    return (
-        <div>
-            
-           <h1> An joke a day keeps the doctor away</h1>
- <button id="chiste">Next Joke</button>
+
+import { useState } from "react";
+import styled from "styled-components"; 
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+
+
+//logica funcion
+
+
+
+const App = () => {
+const [joke, setJoke] = useState(null);
+
+ 
+function nextjoke(){ 
+    fetch('https://icanhazdadjoke.com/slack') 
+    .then(res => res.json()) 
+    .then(res => setJoke(res.attachments[0].text)); 
+ }
+    
+  return ( 
+  <Container>
+
+  <div>
+    <Row>
+           <Col><h1> An joke a day keeps the doctor away</h1></Col>
+    </Row>
+          <Col>  <Button  onClick = {nextjoke}>Next Joke</Button> </Col>
+         
+
+    <Row>
+           <Col>   {joke}    </Col>
+    </Row>
 
         </div>
-    );
-};
+        </Container>
 
 
-// //  document.querySelector('#chiste').addEventListener ('click', () =>{
-//             fetch('https://icanhazdadjoke.com/slack')
-//             .then(res => res.json())
-//             .then(res=> document.querySelector('h3').textContent= res.attachments[0].text); 
-//         });
+)
+}
+
+
+export default App;
+
+
+
         
